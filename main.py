f26aed6b7198bc932695a75cd57c890cb71cb1e1
threedisk <threedisk@gmail.com>
2020-12-29 10:46:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#Write your code below this line 👇
+
+print ("Day 1 - Python Print Function")
+print ("The function is declared like this:")
+print ("print('what to print')")
+
+print("Hello world\nHello World")
+
+print ("Hello "+"Tris")
+
+
